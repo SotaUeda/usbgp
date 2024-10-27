7e93ccf771a0f2530eba126a0f396bd59f6d3ca2
UedaSota <sota.ueda.003@gmail.com>
2024-10-27 14:27:21 +0900

    誤字の修正

--- event/event.go
+++ event/event.go
@@ -2,7 +2,7 @@ package event
 
 // / BGPのRFC内 8.1
 // / (https://datatracker.ietf.org/doc/html/rfc4271#section-8.1)で
-// / 定義されているEvent)を表す列挙型です。
+// / 定義されているEventを表す列挙型です。
 type Event int
 
 //go:generate stringer -type=Event event.go
