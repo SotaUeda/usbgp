e3e493fe29d05d5ecbbc56b12eb24753d8caebac
UedaSota <sota.ueda.003@gmail.com>
2024-10-27 11:03:33 +0900

    TCPConnectionを張るためにテストを修正

--- peer_test.go
+++ peer_test.go
@@ -2,6 +2,7 @@ package peer
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"testing"
 
@@ -9,7 +10,7 @@ import (
 )
 
 func TestTransitionToConnectState(t *testing.T) {
-	config, err := config.NewConfig(
+	cfg, err := config.NewConfig(
 		64512,
 		"127.0.0.1",
 		65413,
@@ -19,14 +20,44 @@ func TestTransitionToConnectState(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to create config: %v", err)
 	}
-	peer := NewPeer(config)
+	peer := NewPeer(cfg)
 	peer.Start()
 	var wg sync.WaitGroup
+	// remote peerを作成
+	errCh := make(chan error)
+	go func() {
+		defer close(errCh)
+		config, err := config.NewConfig(
+			65413,
+			"127.0.0.2",
+			64512,
+			"127.0.0.1",
+			config.Passive,
+		)
+		if err != nil {
+			errCh <- fmt.Errorf("failed to create config: %v", err)
+			return
+		}
+		peer := NewPeer(config)
+		peer.Start()
+		wg.Add(1)
+		go func() {
+			err = peer.Next(context.Background(), &wg)
+			if err != nil {
+				errCh <- fmt.Errorf("failed to handle event: %v", err)
+				return
+			}
+		}()
+		errCh <- nil
+	}()
 	wg.Add(1)
 	go func() { err = peer.Next(context.Background(), &wg) }()
 	wg.Wait()
 	if err != nil {
 		t.Fatalf("failed to handle event: %v", err)
+	}
+	if err := <-errCh; err != nil {
+		t.Fatal(err)
 	}
 	got := peer.State
 	want := Connect
