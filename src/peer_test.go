90ec630fe83b20ba9756581fff7c7bbb96d6503c
UedaSota <sota.ueda.003@gmail.com>
2024-10-31 17:15:26 +0900

    OpenSentに遷移するテストを追加

--- src/peer_test.go
+++ src/peer_test.go
@@ -11,8 +11,6 @@ import (
 	"github.com/SotaUeda/usbgp/config"
 )
 
-// テスト用にPort番号を変更
-// BGPPort = 1790
 var (
 	lp  Peer
 	rp  Peer
@@ -21,6 +19,11 @@ var (
 )
 
 func TestMain(m *testing.M) {
+	// テスト用にPort番号を変更
+	// BGPPort = 1790
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithCancel(context.Background())
+	defer cancel()
 	// テスト用のPeerを作成
 	lcfg, err := config.New(
 		64512,
@@ -33,6 +36,8 @@ func TestMain(m *testing.M) {
 		log.Fatal(err)
 	}
 	lp = *New(lcfg)
+	lp.Start()
+	defer lp.Idle()
 	rcfg, err := config.New(
 		65413,
 		"127.0.0.2",
@@ -44,71 +49,46 @@ func TestMain(m *testing.M) {
 		log.Fatal(err)
 	}
 	rp = *New(rcfg)
-	var cancel context.CancelFunc
-	ctx, cancel = context.WithCancel(context.Background())
-	defer cancel()
+	rp.Start()
+	defer rp.Idle()
 	m.Run()
-	lp.Idle()
-	rp.Idle()
 }
 
 func TestTransitionToConnectState(t *testing.T) {
-	cfg, err := config.New(
-		64512,
-		"127.0.0.1",
-		65413,
-		"127.0.0.2",
-		config.Active,
-	)
-	if err != nil {
-		t.Fatalf("failed to create config: %v", err)
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	p := New(cfg)
-	p.Start()
-	defer p.Idle()
-	var wg sync.WaitGroup
-
-	// remote peerを作成
-	errCh := make(chan error)
-	rCfg, err := config.New(
-		65413,
-		"127.0.0.2",
-		64512,
-		"127.0.0.1",
-		config.Passive,
-	)
-	if err != nil {
-		errCh <- fmt.Errorf("failed to create config: %v", err)
-		return
-	}
-	rP := New(rCfg)
-	defer rP.Idle()
+	// remote peer
+	rErrCh := make(chan error)
+	wg.Add(1)
 	go func() {
-		rP.Start()
-		wg.Add(1)
 		go func() {
-			err = rP.Next(ctx, &wg)
+			err := rp.Next(ctx, &wg)
 			if err != nil {
-				errCh <- fmt.Errorf("failed to handle event: %v", err)
+				rErrCh <- fmt.Errorf("failed to handle event: %v", err)
 				return
 			}
-			errCh <- nil
+			rErrCh <- nil
 		}()
 	}()
 	time.Sleep(1 * time.Second) // remote peerが遷移するよう、1秒待つ
+	// local peer
+	lErrCh := make(chan error)
 	wg.Add(1)
-	go func() { err = p.Next(ctx, &wg) }()
+	go func() {
+		err := lp.Next(ctx, &wg)
+		if err != nil {
+			lErrCh <- fmt.Errorf("failed to handle event: %v", err)
+			return
+		}
+		lErrCh <- nil
+	}()
 	wg.Wait()
-	if err != nil {
-		t.Fatalf("failed to handle event: %v", err)
-	}
-	if err := <-errCh; err != nil {
+	if err := <-rErrCh; err != nil {
 		t.Fatal(err)
 	}
+	if err := <-lErrCh; err != nil {
+		t.Fatalf("failed to handle event: %v", err)
+	}
 
-	got := p.State
+	got := lp.State
 	want := Connect
 	if got != want {
 		t.Fatalf("got %v, want %v", got, want)
@@ -116,70 +96,41 @@ func TestTransitionToConnectState(t *testing.T) {
 }
 
 func TestTransitionToOpenSentState(t *testing.T) {
-	cfg, err := config.New(
-		64512,
-		"127.0.0.3",
-		65413,
-		"127.0.0.4",
-		config.Active,
-	)
-	if err != nil {
-		t.Fatalf("failed to create config: %v", err)
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	p := New(cfg)
-	p.Start()
-	defer p.Idle()
-	var wg sync.WaitGroup
-	// remote peerを作成
-	rCfg, err := config.New(
-		65413,
-		"127.0.0.4",
-		64512,
-		"127.0.0.3",
-		config.Passive,
-	)
-	if err != nil {
-		t.Fatalf("failed to create config: %v", err)
-		return
-	}
-	rP := New(rCfg)
-	defer rP.Idle()
-	errCh := make(chan error)
+	// remote peer
+	rErrCh := make(chan error)
+	wg.Add(1)
 	go func() {
-		rP.Start()
 		go func() {
-			for i := 0; i < 2; i++ {
-				wg.Add(1)
-				err = rP.Next(ctx, &wg)
-				if err != nil {
-					errCh <- fmt.Errorf("failed to handle event: %v", err)
-					return
-				}
+			err := rp.Next(ctx, &wg)
+			if err != nil {
+				rErrCh <- fmt.Errorf("failed to handle event: %v", err)
+				return
 			}
-			errCh <- nil
+			rErrCh <- nil
 		}()
 	}()
-	if err := <-errCh; err != nil {
-		t.Fatal(err)
-	}
+	time.Sleep(1 * time.Second) // remote peerが遷移するよう、1秒待つ
+	// local peer
+	lErrCh := make(chan error)
+	wg.Add(1)
 	go func() {
-		for i := 0; i < 2; i++ {
-			wg.Add(1)
-			err = p.Next(ctx, &wg)
-			if err != nil {
-				errCh <- fmt.Errorf("failed to handle event: %v", err)
-			}
+		err := lp.Next(ctx, &wg)
+		if err != nil {
+			lErrCh <- fmt.Errorf("failed to handle event: %v", err)
+			return
 		}
-		errCh <- nil
+		lErrCh <- nil
 	}()
-	if err := <-errCh; err != nil {
+	wg.Wait()
+	if err := <-rErrCh; err != nil {
 		t.Fatal(err)
 	}
-	wg.Wait()
-	got := p.State
-	want := Connect
+	if err := <-lErrCh; err != nil {
+		t.Fatalf("failed to handle event: %v", err)
+	}
+
+	got := lp.State
+	want := OpenSent
 	if got != want {
 		t.Fatalf("got %v, want %v", got, want)
 	}
